896435e0c6008228778e6bf46c66d2846e11a807
Crofter-dev <shivanshrao1107@gmail.com>
2026-08-27 19:12:38 +0530

    Implement function to group policies by principal
    Added group_policies_by_principal function to categorize policies by user and role.

--- policy.py
+++ policy.py
@@ -26,6 +26,56 @@ def aws_policy(profile: str = None):
                         "Document": version["Document"],
                     })
     return policies
+    
+def group_policies_by_principal(profile: str = None) -> dict:
+    session = boto3.Session(profile_name=profile) if profile else boto3.Session()
+    iam = session.client("iam")
+    policy_documents_by_arn = {}
+    paginator = iam.get_paginator("get_account_authorization_details")
+
+    pages = list(paginator.paginate())  
+
+    for page in pages:
+        for policy in page.get("Policies", []):
+            default_version_id = policy["DefaultVersionId"]
+            for version in policy.get("PolicyVersionList", []):
+                if version["VersionId"] == default_version_id:
+                    policy_documents_by_arn[policy["Arn"]] = version["Document"]
+
+    policies_by_principal = {}
+
+    for page in pages:
+        for user in page.get("UserDetailList", []):
+            principal_name = user["UserName"]
+            docs = []
+
+            for attached in user.get("AttachedManagedPolicies", []):
+                arn = attached["PolicyArn"]
+                if arn in policy_documents_by_arn:
+                    docs.append(policy_documents_by_arn[arn])
+
+            for inline in user.get("UserPolicyList", []):
+                docs.append(inline["PolicyDocument"])
+
+            if docs:
+                policies_by_principal.setdefault(principal_name, []).extend(docs)
+
+        for role in page.get("RoleDetailList", []):
+            principal_name = role["RoleName"]
+            docs = []
+
+            for attached in role.get("AttachedManagedPolicies", []):
+                arn = attached["PolicyArn"]
+                if arn in policy_documents_by_arn:
+                    docs.append(policy_documents_by_arn[arn])
+
+            for inline in role.get("RolePolicyList", []):
+                docs.append(inline["PolicyDocument"])
+
+            if docs:
+                policies_by_principal.setdefault(principal_name, []).extend(docs)
+
+    return policies_by_principal
 
 def normalize(policy_document:dict):
     statements = policy_document.get("Statements", [])
